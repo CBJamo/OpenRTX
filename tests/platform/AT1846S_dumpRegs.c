83be30b3ea855ee85a0e87cd5bf2d5a3c198576a
Federico Amedeo Izzo <federico@izzo.pro>
2021-03-17 20:45:41 +0100

    Add AT1846S register dump test

---
+++ tests/platform/AT1846S_dumpRegs.c
@@ -0,0 +1,60 @@
+/***************************************************************************
+ *   Copyright (C) 2020 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Frederik Saraci IU2NRO                          *
+ *                         Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <interfaces/platform.h>
+#include <interfaces/delays.h>
+#include "hwconfig.h"
+#include "AT1846S.h"
+
+int main(void)
+{
+    platform_init();
+   
+    // Wait for 5 seconds
+    delayMs(5000);
+
+    printf("AT1846S init\r\n");
+    AT1846S_init();
+    
+    printf("AT1846S registers\r\n");
+    uint addr = 0x00;
+    do
+    {
+        uint16_t value = AT1846S_readRegister(addr);
+        printf("reg 0x%02x: 0x%04x\r\n", addr, value);
+        addr++;
+    }
+    while(addr <= 0xFF);
+    
+    while(1);
+}
